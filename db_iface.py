71c76382234f2c24258d2f7844bccd9fae4ba271
NourhanEssam <nourhanessam94@gmail.com>
2016-12-25 11:51:18 +0200

    intersections/streets/traffic lights added

--- db_iface.py
+++ db_iface.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from driver import Driver
+from street import Street
+from trafficlight import Trafficlight
+from intersection import Intersection
 import db_init
 
 app = Flask(__name__)
@@ -23,6 +26,45 @@ def new_driver():
         d.save()
         return redirect(url_for('drivers'))
     return render_template('drivers/new.html')
+
+@app.route('/streets')
+def streets():
+    return render_template("streets/index.html", streets=Street.all())
+
+
+@app.route('/streets/new', methods=['GET', 'POST'])
+def new_street():
+    if request.method == 'POST':
+        d = Street(request.form.to_dict(flat=True))
+        d.save()
+        return redirect(url_for('streets'))
+    return render_template('streets/new.html')
+
+@app.route('/trafficlights')
+def trafficlights():
+    return render_template("trafficlights/index.html", traffic_lights=Trafficlight.all())
+
+
+@app.route('/trafficlights/new', methods=['GET', 'POST'])
+def new_trafficlight():
+    if request.method == 'POST':
+        d = Trafficlight(request.form.to_dict(flat=True))
+        d.save()
+        return redirect(url_for('trafficlights'))
+    return render_template('trafficlights/new.html')
+
+@app.route('/intersections')
+def intersections():
+    return render_template("intersections/index.html", intersections=Intersection.all())
+
+
+@app.route('/intersections/new', methods=['GET', 'POST'])
+def new_intersection():
+    if request.method == 'POST':
+        d = Intersection(request.form.to_dict(flat=True))
+        d.save()
+        return redirect(url_for('intersections'))
+    return render_template('intersections/new.html')
 
 
 @app.errorhandler(404)
---
+++ street.py
@@ -0,0 +1,18 @@
+from model import Model
+import hashlib
+
+
+class Street(Model):
+    def __init__(self, data={}):
+        form_data = data
+        pwd = form_data.pop('password', None)
+        if pwd is not None:
+            hash_object = hashlib.md5(pwd.encode('utf-8'))
+            form_data['encrypted_password'] = hash_object.hexdigest()
+
+        super().__init__(form_data)
+
+
+    @classmethod
+    def table_name(cls):
+        return 'streets'
