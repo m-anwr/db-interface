a7e6353745c66a73d21706d918173c0d6008a8d4
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2016-12-25 19:40:30 +0200

    Removing unused code from models

--- street.py
+++ street.py
@@ -1,18 +1,7 @@
 from model import Model
-import hashlib
 
 
 class Street(Model):
-    def __init__(self, data={}):
-        form_data = data
-        pwd = form_data.pop('password', None)
-        if pwd is not None:
-            hash_object = hashlib.md5(pwd.encode('utf-8'))
-            form_data['encrypted_password'] = hash_object.hexdigest()
-
-        super().__init__(form_data)
-
-
     @classmethod
     def table_name(cls):
         return 'streets'
